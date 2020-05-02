08aad51597aef0e26fba4f4498bffbabeb8f3178
ws2h02 <50068760+ws2h02@users.noreply.github.com>
2020-05-03 00:04:42 +0800

    Update Game.cpp

--- Game.cpp
+++ Game.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <string>
+using namespace std;
 
 void producemine(){
 }
@@ -6,16 +8,66 @@ void producemine(){
 void producegameboard(){
 }
 
+void gamesetting(string diff){
+  int height, width, mines;
+  switch(diff)
+  {
+    case "simple":
+      height = 9;
+      width = 9;
+      mines = 10;
+    case "normal":
+      height = 16;
+      width = 16;
+      mines = 40;
+    case "hard":
+      height = 16;
+      width = 30;
+      mines = 99;
+    case "customized":
+      cout << "Height: ";
+      cin >> height;
+      cout << "Width: ";
+      cin >> width;
+      cout << "Mines: ";
+      cin >> mines;
+    default:
+      cout << "Invalid input" << endl;
+      cout << "Reinput the difficulty: ";
+      cin >> diff;
+      gamesetting(diff);
+    producegameboard(int height, int width, int mines);
+  }
+}
+
 void printgameboard(){
 }
 
-void scaninput(){
+void scaninput(string player_input){
 }
 
-
+bool keepon(){
+}
+  
 int main(){
   // player input: difficulty(number? or string?[simple/normal/hard/customized]), size(one number?[square], two number[rectangle]), number of mines[if player choose customized]
-  // system output: gameboard
-  // player input: command (game control, save and load)
+  string diff,player_input;
+  bool goingon = true
+  cout << "Please input the game setting:" >> endl;
+  cout << "Difficulty: ";
+  cin >> diff;
+  gamesetting(diff);
   // repeat step 2 and 3 until player win
+  while( goingon() ){
+  // system output: gameboard
+    printgameboard();
+  // player input: command (game control, save and load)
+    cout << "Please input a command: ";
+    cin >> player_input;
+    scaninput(player_input);
+    goingon = keepon();
+  }
+  // system output: gameboard, win or lose
+  
+  
 }
