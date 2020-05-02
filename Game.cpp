7cb15536a889fec3566830318e5e4a7366cfebde
ws2h02 <50068760+ws2h02@users.noreply.github.com>
2020-05-02 18:49:25 +0800

    Update Game.cpp

--- Game.cpp
+++ Game.cpp
@@ -1,15 +1,21 @@
 #include <iostream>
 
-void producegameboard(){
-}
-
 void producemine(){
 }
 
+void producegameboard(){
+}
 
+void printgameboard(){
+}
 
+void scaninput(){
+}
 
 
 int main(){
-  
+  // player input: difficulty(number? or string?[simple/normal/hard/customized]), size(one number?[square], two number[rectangle]), number of mines[if player choose customized]
+  // system output: gameboard
+  // player input: command (game control, save and load)
+  // repeat step 2 and 3 until player win
 }
